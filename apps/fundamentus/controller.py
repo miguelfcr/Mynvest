511eeeacd4655bc257ecc22e6840b6bbd6100cdd
Miguel Rodrigues <miguel.fcr@hotmail.com>
2019-09-29 22:52:01 -0300

    Implementado SQLAlchemy

--- apps/fundamentus/controller.py
+++ apps/fundamentus/controller.py
@@ -1,26 +1,40 @@
 import pandas as pd
+from datetime import datetime
+from pprint import pprint
 
 from apps.fundamentus.webservice import WebFundamentus as WF
-from apps.fundamentus.model import Model
+from apps.fundamentus.model import Controll, Ativo, Balanco, Demonstrativo, Indicadores
 
 class Controller:
 	def __init__(self):
-		self.Model = Model()
+		self.Controll = Controll()
 
-	def atualizalistaativos(self):
-		ativos_list = WF().getativolist().rename(columns={"Papel": "acao", "Nome Comercial": "nome"}).to_dict(orient='records')
-		self.Model.inserelistaativos(ativos_list)
+	def atualiza_lista_ativos(self):
+		ativos_list = WF().getativolist().rename(columns={"Papel": "acao", "Nome Comercial": "nome_empresa"}).to_dict(orient='records')
+		insert_list = []
+
+		ativos_list = [ativos_list[60]]
+
+		try:
+			for ativo_dict in ativos_list:
+				ObjAtivo = self._get_ativo(ativo_dict['acao'])
+				insert_list.append(ObjAtivo)
+
+			self.Controll.insert(insert_list)
+		except Exception as e:
+			print(e)
 		
-	def atualizaativo(self, papel):
+	def _get_ativo(self, papel):
 		table_list = WF().getativo(papel)
 
-		ativo_dict = self._preparacabecalho(table_list[0], table_list[1])
-		ativo_dict['indicador_dict'] = self._preparaindicadores(1, table_list[2])
-		ativo_dict['balanco_dict'] = self._preparabalanco(1, table_list[3])
-		ativo_dict['demonstrativo_dict'] = self._preparademonstrativo(1, table_list[4])
-		return ativo_dict
+		ativo_dict = self._prepara_cabecalho(table_list[0], table_list[1])
+		ativo_dict['indicadores'] = [self._prepara_indicadores(table_list[2])]
+		ativo_dict['balanco'] = [self._prepara_balanco(table_list[3])]
+		ativo_dict['demonstrativo'] = [self._prepara_demonstrativo(table_list[4])]
+		
+		return Ativo(**ativo_dict)
 
-	def _formatacampo(self, data, campo=''):
+	def _formata_campo(self, data, campo=''):
 		newdata = data
 		try:
 			if type(data) == int:
@@ -32,8 +46,7 @@ class Controller:
 			elif data.count(',') and data.count('%'):
 				newdata = float(data.replace(',','.').replace('%',''))
 			elif data.count('/'):
-				data = data.split('/')
-				newdata = data[2] + '-' + data[1] + '-' + data[0]
+				newdata = data  # Funciona sem formatar a data '-'
 			elif data.isnumeric():
 				newdata = float(data) / 100
 		except Exception as e:
@@ -41,121 +54,52 @@ class Controller:
 
 		return newdata
 
-	def _preparacabecalho(self, cab0, cab1):
-		cabecalho0 = cab0[1].rename(index={0:'acao', 3:'setor', 4:'subsetor'})
-		cabecalho1 = cab0[3].rename(index={0:'cotacao', 1:'dtultcotacao'})
-		cabecalho2 = cab1[1].rename(index={0:'vlmercado', 1:'vlfirma'})
-		cabecalho3 = cab1[3].rename(index={0:'dtultbalanco', 1:'nracoes'})
+	def _prepara_cabecalho(self, cab0, cab1):
+		cabecalho_dict = cab0[1].rename(index={0:'acao', 2:'nome_empresa', 3:'setor', 4:'subsetor'}).to_dict()
+		cabecalho_dict.update(cab0[3].rename(index={0:'cotacao', 1:'data_ultima_cotacao'}).to_dict())
+		cabecalho_dict.update(cab1[1].rename(index={0:'valor_mercado', 1:'valor_firma'}).to_dict())
+		cabecalho_dict.update(cab1[3].rename(index={0:'data_ultimo_balanco', 1:'numero_acoes'}).to_dict())
 
-		cabecalho_dict = cabecalho0.to_dict()
-		cabecalho_dict.update(cabecalho1.to_dict())
-		cabecalho_dict.update(cabecalho2.to_dict())
-		cabecalho_dict.update(cabecalho3.to_dict())
-		return {k: self._formatacampo(v, k) for (k,v) in cabecalho_dict.items() if type(k) == str}
+		return {k: self._formata_campo(v, k) for (k,v) in cabecalho_dict.items() if type(k) == str}
 
-		sql_update = """UPDATE ativo set 
-					setor = '%(setor)s',
-					subsetor = '%(subsetor)s',
-					cotacao = %(cotacao)s,
-					vlmercado = %(vlmercado)s,
-					vlfirma = %(vlmercado)s,
-					nracoes = %(nracoes)s,
-					dtultcotacao = %(dtultcotacao)s,
-					dtultbalanco = %(dtultbalanco)s 
-					WHERE acao = '%(acao)s'""" % cabecalho_dict
-		print(sql_update)
+	def _prepara_balanco(self, bal):
+		balanco_dict = bal[1].rename(index={1:'valor_ativo', 2:'disponibilidades', 3:'ativo_circulante'}).to_dict()
+		balanco_dict.update(bal[3].rename(index={1:'divida_bruta', 2:'divida_liquida', 3:'patrimonio_liquido'}).to_dict())
+		
+		balanco_dict = {k: self._formata_campo(v, k) for (k,v) in balanco_dict.items() if type(k) == str}
+		return Balanco(**balanco_dict)
 
-	def _preparabalanco(self, idativo, bal):
-		balanco0 = bal[1].rename(index={1:'ativo', 2:'disponibilidades', 3:'ativocirculante'})
-		balanco1 = bal[3].rename(index={1:'divbruta', 2:'divliquida', 3:'patrliq'})
+	def _prepara_demonstrativo(self, dem):
+		demonstrativo_dict = dem[1].rename(index={2:'receita_liquida_12', 3:'ebit_12', 4:'lucro_liquido_12'}).to_dict()
+		demonstrativo_dict.update(dem[3].rename(index={2:'receita_liquida_3',  3:'ebit_3',  4:'lucro_liquido_3'}).to_dict())
 
-		balanco_dict = {"idativo": idativo}
-		balanco_dict.update(balanco0.to_dict())
-		balanco_dict.update(balanco1.to_dict())
-		return {k: self._formatacampo(v, k) for (k,v) in balanco_dict.items() if type(k) == str}
+		demonstrativo_dict = {k: self._formata_campo(v, k) for (k,v) in demonstrativo_dict.items() if type(k) == str}
+		return Demonstrativo(**demonstrativo_dict)
 
-		sql_insert = """INSERT INTO balanco (idativo, ativo, disponibilidades, ativocirculante, divbruta, divliquida, patrliq)
-						VALUES (%(idativo)s,
-								%(ativo)s,
-								%(disponibilidades)s,
-								%(ativocirculante)s,
-								%(divbruta)s,
-								%(divliquida)s,
-								%(patrliq)s)
-					 """ % balanco_dict
-		print(sql_insert)
+	def _prepara_indicadores(self, ind):
+		indicadores_dict = ind[3].rename(index={
+									1: 'p_l', 7: 'p_ativ_circ_liq',
+									2: 'p_vp', 8: 'div_yield', 
+									3: 'p_ebit', 9: 'ev_ebit', 
+									4: 'psr', 10: 'giro_ativos',
+									5: 'p_ativos', 11: 'cres_rec5',
+									6: 'p_cap_giro'}).to_dict()
 
-	def _preparademonstrativo(self, idativo, dem):
-		demonstrativo0 = dem[1].rename(index={2:'recliq12', 3:'ebit12', 4:'lucroliq12'})
-		demonstrativo1 = dem[3].rename(index={2:'recliq3',  3:'ebit3',  4:'lucroliq3'})
+		indicadores_dict.update(
+								ind[5].rename(index={
+									1: 'lpa', 6: 'ebit_ativo',
+									2: 'vpa', 7: 'roic',
+									3: 'marg_bruta', 8: 'roe',
+									4: 'marg_ebit', 9: 'liquidez_corr',
+									5: 'marg_liquida', 10: 'div_bruta_patrim',
+								}).to_dict()
+							)
 
-		demonstrativo_dict = {"idativo": idativo}
-		demonstrativo_dict.update(demonstrativo0.to_dict())
-		demonstrativo_dict.update(demonstrativo1.to_dict())
-		return {k: self._formatacampo(v, k) for (k,v) in demonstrativo_dict.items() if type(k) == str}
 
-		sql_insert = """INSERT INTO demonstrativo (idativo, recliq12, ebit12, lucroliq12, recliq3, ebit3, lucroliq3)
-						VALUES (%(idativo)s,
-								%(recliq12)s,
-								%(ebit12)s,
-								%(lucroliq12)s,
-								%(recliq3)s,
-								%(ebit3)s,
-								%(lucroliq3)s)
-					 """ % demonstrativo_dict
-		print(sql_insert)
-
-	def _preparaindicadores(self, idativo, ind):
-		indicador0 = ind[3].rename(index={
-			1: 'p_l', 7: 'p_ativcircliq',
-			2: 'p_vp', 8: 'divyield',  
-			3: 'p_ebit', 9: 'ev_ebit', 
-			4: 'psr', 10: 'giroativos',
-			5: 'p_ativos', 11: 'cres_rec',
-			6: 'p_capgiro',
-		})
-		indicador1 = ind[5].rename(index={
-			1: 'lpa', 6: 'ebit_ativo',
-			2: 'vpa', 7: 'roic',
-			3: 'margbruta', 8: 'roe',
-			4: 'margebit', 9: 'liquidezcorr',
-			5: 'margliquida', 10: 'divbr_patrim',
-		})
-
-		indicador_dict = {"idativo": idativo}
-		indicador_dict.update(indicador0.to_dict())
-		indicador_dict.update(indicador1.to_dict())
-		return {k: self._formatacampo(v, k) for (k,v) in indicador_dict.items() if type(k) == str}
-
-		sql_insert = """INSERT INTO indicadores (idativo, p_l, p_vp, p_ebit, psr, p_ativos, p_capgiro, p_ativcircliq, 
-												divyield, ev_ebit, giroativos, cres_rec, lpa, vpa, margbruta, margebit, 
-												margliquida, ebit_ativo, roic, roe, liquidezcorr, divbr_patrim)
-						VALUES (%(idativo)s,
-								%(p_l)s,
-								%(p_vp)s,
-								%(p_ebit)s,
-								%(psr)s,
-								%(p_ativos)s,
-								%(p_capgiro)s,
-								%(p_ativcircliq)s,
-								%(divyield)s,
-								%(ev_ebit)s,
-								%(giroativos)s,
-								%(cres_rec)s,
-								%(lpa)s,
-								%(vpa)s,
-								%(margbruta)s,
-								%(margebit)s,
-								%(margliquida)s,
-								%(ebit_ativo)s,
-								%(roic)s,
-								%(roe)s,
-								%(liquidezcorr)s,
-								%(divbr_patrim)s)
-					 """ % indicador_dict
-		print(sql_insert)
+		indicadores_dict = {k: self._formata_campo(v, k) for (k,v) in indicadores_dict.items() if type(k) == str}
+		return Indicadores(**indicadores_dict)
 
 
 if __name__ == "__main__":
 	C = Controller()
-	C.atualizalistaativos()
+	C.atualiza_lista_ativos()
--- apps/fundamentus/model.py
+++ apps/fundamentus/model.py
@@ -1,23 +1,103 @@
-from db.connection import DB
+from sqlalchemy import Column, Integer, String, Date, Float, BigInteger, ForeignKey, Sequence, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
 
-class Model(object):
+_Base = declarative_base()
 
-	def __new__(cls, *args, **kwargs):
-		if not hasattr(cls, '_instance'):
-			cls._instance = super(Model, cls).__new__(cls, *args, **kwargs)
-		return cls._instance
-
+class Controll:
 	def __init__(self):
-		self.db = DB()
+		database_uri = 'postgres+psycopg2://postgres:postgres@localhost:5432/mynvest'
+		self.engine = create_engine(database_uri)
+		self.Session = sessionmaker(bind=self.engine)
 
-	def inserelistaativos(self, ativo_list):
-		for ativo_dict in ativo_list:
-			try:
-				sql_insert = "INSERT INTO ativo (acao, nomeempresa)"\
-						"VALUES (%(acao)s, %(nome)s)" % ativo_dict
-				print(sql_insert)
-			except Exception as e:
-				print(e)
+	def recreate_database(self):
+		_Base.metadata.drop_all(self.engine)
+		_Base.metadata.create_all(self.engine)
+
+	def insert(self, object_list):
+		session = self.Session()
+		for obj in object_list:
+			session.add(obj)
+		session.commit()
+		session.close()
+
+class Ativo(_Base):
+	__tablename__ = 'ativo'
+	id = Column(Integer, Sequence('ativo_id_seq'), primary_key=True)
+	acao = Column(String, unique=True, nullable=False, index=True)
+	nome_empresa = Column(String)
+	setor = Column(String)
+	subsetor = Column(String)
+	cotacao = Column(Float)
+	valor_mercado = Column(BigInteger)
+	valor_firma = Column(BigInteger)
+	numero_acoes = Column(BigInteger)
+	data_ultima_cotacao = Column(Date)
+	data_ultimo_balanco = Column(Date)
+
+	def __repr__(self):
+		return "<Ativo(id='{}', acao='{}', nome empresa={}, setor={}, subsetor={}, cotacao={}, valor mercado={}, valor firma={}, "\
+				"numero acoes={}, data ultima cotacao={}, data ultimo balanco={} "\
+				.format(self.id, self.acao, self.nome_empresa, self.setor, self.subsetor, self.cotacao, self.valor_mercado, 
+						self.valor_firma, self.numero_acoes, self.data_ultima_cotacao, self.data_ultimo_balanco)
+
+class Balanco(_Base):
+	__tablename__ = 'balanco'
+	id = Column(Integer, Sequence('balanco_id_seq'), primary_key=True)
+	ativo_id = Column(Integer, ForeignKey('ativo.id'))
+	valor_ativo = Column(Float)
+	disponibilidades = Column(Float)
+	ativo_circulante = Column(Float)
+	divida_bruta = Column(Float)
+	divida_liquida = Column(Float)
+	patrimonio_liquido = Column(Float)
+
+class Demonstrativo(_Base):
+	__tablename__ = 'demonstrativo'
+	id = Column(Integer, Sequence('demonstrativo_id_seq'), primary_key=True)
+	ativo_id = Column(Integer, ForeignKey('ativo.id'))
+	receita_liquida_12 = Column(Float)
+	ebit_12 = Column(Float)
+	lucro_liquido_12 = Column(Float)
+	receita_liquida_3 = Column(Float)
+	ebit_3 = Column(Float)
+	lucro_liquido_3 = Column(Float)
+
+class Indicadores(_Base):
+	__tablename__ = 'indicadores'
+	id = Column(Integer, Sequence('indicadores_id_seq'), primary_key=True)
+	ativo_id = Column(Integer, ForeignKey('ativo.id'))
+	p_l = Column(Float)
+	p_vp = Column(Float)
+	p_ebit = Column(Float)
+	psr = Column(Float)
+	p_ativos = Column(Float)
+	p_cap_giro = Column(Float)
+	p_ativ_circ_liq = Column(Float)
+	div_yield = Column(Float)
+	ev_ebit = Column(Float)
+	giro_ativos = Column(Float)
+	cres_rec5 = Column(Float)
+	lpa = Column(Float)
+	vpa = Column(Float)
+	marg_bruta = Column(Float)
+	marg_ebit = Column(Float)
+	marg_liquida = Column(Float)
+	ebit_ativo = Column(Float)
+	roic = Column(Float)
+	roe = Column(Float)
+	liquidez_corr = Column(Float)
+	div_bruta_patrim = Column(Float)
+
+# Cria os relacionamentos das tabelas
+Balanco.ativo = relationship("Ativo", back_populates="balanco")
+Demonstrativo.ativo = relationship("Ativo", back_populates="demonstrativo")
+Indicadores.ativo = relationship("Ativo", back_populates="indicadores")
+
+Ativo.indicadores = relationship("Indicadores", order_by=Indicadores.id, back_populates="ativo")
+Ativo.balanco = relationship("Balanco", order_by=Balanco.id, back_populates="ativo")
+Ativo.demonstrativo = relationship("Demonstrativo", order_by=Demonstrativo.id, back_populates="ativo")
 
 if __name__ == "__main__":
-	M = Model()
+	C = Controll()
+	C.recreate_database()
